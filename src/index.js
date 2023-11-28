065e92fc95cb92a6d0c2c3ce29261a59679e60a9
Arian N. Yamchelo <ariannajafi@icloud.com>
2023-11-28 20:12:13 +1100

    Setup environment and dependencies.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+// Import styles
+import './static/scss/main.scss'
+
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// Import Bootstrap JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+// Import Shoelace Light theme CSS
+import '@shoelace-style/shoelace/dist/themes/light.css'
+
+// Import Shoelace JS
+import '@shoelace-style/shoelace/dist/shoelace.js';
