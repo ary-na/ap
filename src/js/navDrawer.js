06921c6870b96c3abd6db949fe86303e72b3e882
Arian N. Yamchelo <ariannajafi@icloud.com>
2023-11-30 23:16:43 +1100

    Minor adjustments to the comments.

--- src/js/navDrawer.js
+++ src/js/navDrawer.js
@@ -2,8 +2,8 @@ const slIconButton = document.querySelector('nav > div > sl-icon-button')
 const slDrawer = document.querySelector('nav > sl-drawer')
 const drawerItems = document.querySelectorAll('sl-drawer > ul > li a')
 
-// Show drawer on hamburger menu icon click.
+// Show drawer on drawer icon click.
 slIconButton.addEventListener('click', () => slDrawer.show())
 
-// Hide drawer on menu item click.
+// Hide drawer on drawer item click.
 drawerItems.forEach(item => item.addEventListener('click', async () => await slDrawer.hide()))
