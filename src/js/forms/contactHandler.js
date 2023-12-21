12e8017f540f206a428f6fc66af34c1c0f7583e6
Arian N. Yamchelo <ariannajafi@icloud.com>
2023-12-21 18:45:53 +1100

    Update contactHandler.js to display confirmation message.

--- src/js/forms/contactHandler.js
+++ src/js/forms/contactHandler.js
@@ -10,26 +10,28 @@ document.querySelector('form').addEventListener('submit', async e => {
         const formData = new FormData(e.target);
         try {
             await Contact.postContact(formData)
-            await createDialog('success', 'done')
+            await resetForm()
+            await createDialog(`I have received your message!`, `Thank you for contacting me ${formData.get('name')}. I have sent you an email with the message details. Please feel free to reply to the email with any additional information or send me another message.`)
         } catch (err) {
-            console.log('Error posting contact:', err)
-            await createDialog('success', 'done')
+            await createDialog('Sorry, your message could not be sent!', 'There was an error sending your message. Please reload the page and try again.')
         }
     }
     submitBtn.removeAttribute('loading')
 })
 
 let createDialog = async (label, content) => {
-    const dialogEl = document.createElement('sl-dialog');
-    dialogEl.setAttribute('label', label);
+    const dialogEl = document.createElement('sl-dialog')
+    dialogEl.setAttribute('style', '--width: 50vw;')
+    dialogEl.setAttribute('label', label)
 
     // Create dialog content
     const contentEl = document.createElement('p')
+    contentEl.classList.add('lh-lg')
     contentEl.innerHTML = content
     dialogEl.appendChild(contentEl)
 
     // Append dialog to the body
-    await document.body.appendChild(dialogEl);
+    await document.body.appendChild(dialogEl)
 
     // Show the dialog
     dialogEl.show();
@@ -38,4 +40,10 @@ let createDialog = async (label, content) => {
     dialogEl.addEventListener('sl-after-hide', () => {
         dialogEl.remove()
     })
+}
+
+let resetForm = async () => {
+    document.querySelector('#name').value = ''
+    document.querySelector('#email').value = ''
+    document.querySelector('#message').value = ''
 }
